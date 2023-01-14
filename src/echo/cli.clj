6cd3190821c56293b55c50a4811114d2ce47c1c8
Adrian Smith <sfyire@gmail.com>
2023-01-14 12:41:36 +0000

    Adds functions for probing mssql instances

--- src/echo/cli.clj
+++ src/echo/cli.clj
@@ -3,19 +3,26 @@
 
 
 (defn powershell
+  ([args]
+   (powershell args {}))
+  ([args options]
+   (run-command "powershell" args (merge {:throw false
+                                          :timeout 5000} options))))
+
+
+(defn reg
   [args]
-  (run-command "powershell" args {:throw false}))
-
-
-(defn quote
-  [x]
-  (str \\ \" \"  x \" \\ \"))
-
-
-(defn reg-query
-  [args]
-  (powershell (into ["REG" "QUERY"] (mapv quote args))))
+  (powershell (into ["REG"] args)))
 
 
 (comment
-  (reg-query ["HKLM\\SOFTWARE\\MICROSOFT\\Microsoft SQL Server"]))
+  (reg ["QUERY" "'HKLM\\SOFTWARE\\MICROSOFT\\Microsoft SQL Server'"]))
+
+
+(defn sqlcmd
+  [args]
+  (powershell (into ["sqlcmd"] args)))
+
+
+(comment
+  (sqlcmd ["-S" "localhost\\sqlexpress" "-Q" "'SELECT @@VERSION'"]))
--- src/echo/domain.clj
+++ src/echo/domain.clj
@@ -11,7 +11,12 @@
 
 (defn mssql-exists?
   []
-  (not= "" (cli/reg-query ["HKLM\\SOFTWARE\\MICROSOFT\\Microsoft SQL Server"])))
+  (not= "" (cli/reg ["QUERY" "'HKLM\\SOFTWARE\\MICROSOFT\\Microsoft SQL Server'"])))
+
+
+(defn mssql-instance-connectable?
+  [name]
+  (not= "" (cli/sqlcmd ["-S" (str "localhost" \\ name) "-Q" "'SELECT @@VERSION'"])))
 
 
 (defn next-action

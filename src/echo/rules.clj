6f7ed6e43f1ac57a74ad3bb7213c3136452c816a
Adrian Smith <sfyire@gmail.com>
2023-01-31 22:20:59 +0000

    Fix regressions

--- src/echo/rules.clj
+++ src/echo/rules.clj
@@ -1,7 +1,6 @@
 (ns echo.rules
   (:require [clara.rules :refer [fire-rules insert-all mk-session]]
             [clara.tools.inspect :as inspect]
-            [echo.queries]
             [echo.rules.config]
             [echo.rules.oneperone]
             [echo.rules.mssql]))
@@ -13,7 +12,6 @@
    (-> (mk-session 'echo.rules.config
                    'echo.rules.oneperone
                    'echo.rules.mssql
-                   'echo.queries
                    :fact-type-fn :fact)
        (insert-all facts)
        (fire-rules))))
@@ -22,9 +20,9 @@
 (defn fire
   []
   (-> (session)
-      (inspect/inspect)
-      :fact->explanations
-      keys))
+      (inspect/inspect)))
+      ;:fact->explanations
+      ;keys))
 
 
 (defn explain
